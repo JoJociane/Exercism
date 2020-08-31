8fdf8d3776e561114be8e777afd491f355bcbc14
Jociane Costa E Silva <jociane.silva@hbsis.com.br>
2020-08-31 10:57:36 -0300

    Exercício Resistor (Array)

--- teams/padawan-2020-maringa/csharp/resistor-color/ResistorColor.cs
+++ teams/padawan-2020-maringa/csharp/resistor-color/ResistorColor.cs
@@ -1,14 +1,24 @@
 ﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.PortableExecutable;
 
 public static class ResistorColor
 {
     public static int ColorCode(string color)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var list = Colors(); int cor = -1;
+        for (int i = 0; i < list.Length; i++)
+        {
+            if (list[i] == color)
+                cor = i;
+        }
+        return cor;
     }
 
     public static string[] Colors()
     {
-        throw new NotImplementedException("You need to implement this function.");
+         return new [] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+        
     }
 }
--- teams/padawan-2020-maringa/csharp/two-fer/TwoFer.cs
+++ teams/padawan-2020-maringa/csharp/two-fer/TwoFer.cs
@@ -4,12 +4,12 @@ public static class TwoFer
 {
     // In order to get the tests running, first you need to make sure the Speak method 
     // can be called both without any arguments and also by passing one string argument.
-    public static string Speak()
+   /* public static string Speak()
     {
-        return Speak("you");
-    }
+        return Speak("you");// chamei a sobrecarga
+    }*/
 
-    public static string Speak(string name)
+    public static string Speak(string name = "you")
     {
         return ($"One for {name}, one for me.");
     }
